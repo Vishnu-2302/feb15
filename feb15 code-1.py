9bd56fac291184e182313629426daed724241299
Vishnu-2302 <99253009+Vishnu-2302@users.noreply.github.com>
2022-02-15 20:06:26 +0530

    Add files via upload

---
+++ feb15 code-1.py
@@ -0,0 +1,6 @@
+a=int(input('enter the value of a : '))
+b=int(input('enter the value of b : '))
+x=a//b
+y=a/b
+print(f'{a} // {b} = {x} *Integer divison')
+print(f'{a} / {b} = {y} *Floor division')
---
+++ feb15 code-2.py
@@ -0,0 +1,8 @@
+a=int(input('enter the value of a : '))
+b=int(input('enter the value of b : '))
+x=a+b
+y=a-b
+z=a*b
+print(f'{a} + {b} = {x}')
+print(f'{a} - {b} = {y}')
+print(f'{a} * {b} = {z}')
